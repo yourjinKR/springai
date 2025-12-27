e2166a82c9fd6add0dfe475ac3517385caf5bd4f
yourjin <155814455+yourjinKR@users.noreply.github.com>
2025-12-27 17:46:52 +0900

    init : 프로젝트 생성

---
+++ src/test/java/app/springai/SpringaiApplicationTests.java
@@ -0,0 +1,13 @@
+package app.springai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
